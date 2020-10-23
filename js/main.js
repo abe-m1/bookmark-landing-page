804d8e40ead81b35714ac84f85464ad746d481b2
abe menashy <abestbet1@gmail.com>
2020-10-22 20:27:18 -0700

    js fixes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,26 @@
-//If there are more than one
-console.log('loaded js');
 const tabItems = document.querySelectorAll('.tab-item');
-console.log(tabItems);
 const tabContentItems = document.querySelectorAll('.tab-content-item');
-console.log(tabContentItems);
 
-// Select tab content item
 function selectItem(e) {
-  console.log('called select item', this);
-  // Remove all show and border classes
   removeBorder();
   removeShow();
-  // Add border to current tab item
   this.classList.add('tab-border');
-  // Grab content item from DOM
   const tabContentItem = document.querySelector(`#${this.id}-content`);
-  // Add show class
   tabContentItem.classList.add('show');
 }
 
-// Remove bottom borders from all tab items
 function removeBorder() {
   tabItems.forEach((item) => {
     item.classList.remove('tab-border');
   });
 }
 
-// Remove show class from all content items
 function removeShow() {
   tabContentItems.forEach((item) => {
     item.classList.remove('show');
   });
 }
 
-// Listen for tab item click
 tabItems.forEach((item) => {
   item.addEventListener('click', selectItem);
 });
@@ -42,13 +29,26 @@ var acc = document.getElementsByClassName('accordion');
 var i;
 
 for (i = 0; i < acc.length; i++) {
+  let propName = `svg-icon-${i}`;
+  let arrowContainer = acc[i].children[0];
   acc[i].addEventListener('click', function () {
+    var arrow = document.getElementById(propName);
     this.classList.toggle('active');
+
     var panel = this.nextElementSibling;
+    console.log(arrowContainer);
     if (panel.style.display === 'block') {
       panel.style.display = 'none';
+      arrow.style.stroke = 'hsl(231, 69%, 60%)';
+      // arrowContainer.style.transform = 'rotate(-180deg)';
+      arrowContainer.classList.remove('accordian-active');
     } else {
       panel.style.display = 'block';
+
+      arrow.style.stroke = 'hsl(0, 94%, 66%)';
+      console.log(arrowContainer);
+
+      arrowContainer.classList.add('accordian-active');
     }
   });
 }
