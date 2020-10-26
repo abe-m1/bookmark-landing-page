4ba8033e17703123cf9c77d1ff7d209741bcc780
abe menashy <abestbet1@gmail.com>
2020-10-25 21:24:38 -0700

    mobile navigation

--- js/main.js
+++ js/main.js
@@ -78,3 +78,13 @@ function validate(e) {
 }
 
 submitButton.addEventListener('click', validate);
+
+const navButton = document.querySelector('#nav-button');
+navButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('click', navButton.children[0].style);
+  navButton.children[0].classList.toggle('hide');
+  // navButton.children[1].classList.toggle('show');
+
+  document.querySelector('.nav-container').classList.toggle('show');
+});
